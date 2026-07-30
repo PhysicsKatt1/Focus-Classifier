b792bbeb5944e4ba3d8b82a497be8e7d3f6eeef8
PhysicsKatt1 <ka.kaylegian@gmail.com>
2026-07-30 11:00:05 -0700

    Updated and cleaned for a secondary training set in RegressionMod.py

--- RegressionMod.py
+++ RegressionMod.py
@@ -25,7 +25,7 @@ csv_file_val = path + '/RegressionData_30kV_0.09nA_val/labels.csv'
 batch = 19
 learning_rate = 1e-2
 mod_name = '2.0'
-epochs = 10
+epochs = 15
 
 USE_PROFILER = False
 
@@ -66,20 +66,27 @@ def create_datasets():
     train_size = int(0.8 * len(dataset))
     val_size = len(dataset) - train_size
 
-    train_size1 = int(0.8 * len(dataset1))
+    train_size1 = int(0.5 * len(dataset1))
     val_size1= len(dataset1) - train_size1
     
     #------ create random samplers to ensure even data representation ------#
-    train_dataset, val_dataset0 = random_split( dataset, [train_size, val_size], generator = generator)
-    _, val_dataset1 = random_split(dataset1, [val_size1, train_size1], generator = generator)
+    train_dataset0, val_dataset0 = random_split(dataset, [train_size, val_size], generator = generator)
+    train_dataset1, val_dataset1 = random_split(dataset1, [train_size1, val_size1], generator = generator)
     val_dataset = ConcatDataset([val_dataset0, val_dataset1])
+    train_dataset = ConcatDataset([train_dataset0, train_dataset1])
     
-    train_labels = dataset.data.iloc[train_dataset.indices]['Defocus']
-    train_counts = train_labels.value_counts()
-    train_weights = train_labels.map(lambda x: 1.0 / np.sqrt(train_counts[x])).values
+    train_labels0 = dataset.data.iloc[train_dataset0.indices]['Defocus']
+    train_counts0 = train_labels0.value_counts()
+    train_weights0 = train_labels0.map(lambda x: 1.0 / np.sqrt(train_counts0[x])).values
+
+    train_labels1 = dataset1.data.iloc[train_dataset1.indices]['Defocus']
+    train_counts1 = train_labels1.value_counts()
+    train_weights1 = train_labels1.map(lambda x: 1.0 / np.sqrt(train_counts1[x])).values
+    
+    train_weights = ConcatDataset([train_weights0, train_weights1])
     sampler_train = WeightedRandomSampler(weights = torch.DoubleTensor(train_weights), num_samples=len(train_dataset), replacement = True)
 
-    val_labels0 = dataset.data.iloc[val_dataset.indices]['Defocus']
+    val_labels0 = dataset.data.iloc[val_dataset0.indices]['Defocus']
     val_counts0 = val_labels0.value_counts()
     val_weights0 = val_labels0.map(lambda x: 1.0 / np.sqrt(val_counts0[x])).values
 
